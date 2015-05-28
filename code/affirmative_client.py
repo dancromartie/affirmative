4550a112c933009e7fd00233233f1766c13f1b14
Dan Cromartie <cromartied@gmail.com>
2015-05-27 21:00:33 -0400

    little fixes

--- code/affirmative_client.py
+++ code/affirmative_client.py
@@ -1,19 +1,15 @@
 import requests
 import json
 import random
-from datetime import datetime
 import time
 
 class Client:
 
-    def __init__(self, url, environment, verify_cert=True):
+    def __init__(self, url, verify_cert=True):
         self.storage_url = url + "/affirmative/store"
-        self.environment = environment
         self.verify_cert = verify_cert
 
     def affirm_one(self, name, data):
-        utc_datetime = datetime.utcnow()
-        now_ts = utc_datetime.strftime("%Y-%m-%d %H:%M:%S.%f")
         payload = {
             "events": [
                 {"name": name, "time": time.time(), "data": data}
--- code/run_client_test.py
+++ code/run_client_test.py
@@ -2,7 +2,7 @@ import time
 
 import affirmative_client
 
-ac = affirmative_client.Client("http://localhost:5123", "prod")
+ac = affirmative_client.Client("http://localhost:5123", verify_cert=False)
 for i in range(1, 100):
     print "sending!"
     ac.affirm_one("that_critical_app_health_check", "good")
